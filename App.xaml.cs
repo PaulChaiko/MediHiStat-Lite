d3c7e35918189e52f4175827de3e2ac9c113866c
PaulChaiko <pavel.chaiko9@gmail.com>
2026-09-19 08:57:41 +0500

    chore: remove unused application imports

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.Data.Sqlite;
-using System.Configuration;
-using System.Data;
 using System.Windows;
 using System.Windows.Controls.Primitives;
 using System.Windows.Media.Animation;
